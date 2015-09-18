93ec0c51e8ab56d4fcfa375d065700d5e9b1934b
leslie.li <leslie.li@live.com>
2015-09-18 11:10:27 +0800

    init zhucexia project

---
+++ src/main/java/com/keji50/zhucexia/dao/po/UserPo.java
@@ -0,0 +1,33 @@
+package com.keji50.zhucexia.dao.po;
+
+import lombok.Data;
+
+/**
+ * 网站会员对象
+ *
+ * @author   chao.li
+ * @version  
+ * @since    Ver 1.1
+ * @Date	 2015年9月18日		上午11:04:03
+ *
+ * @see 	 
+ */
+@Data
+public class UserPo {
+    
+    // 会员ID
+    private Integer id;
+    
+    // 用户名
+    private String username;
+    
+    // 密码
+    private String password;
+    
+    // 手机号
+    private String phoneNumber;
+    
+    // 邮箱
+    private String email;
+}
+
