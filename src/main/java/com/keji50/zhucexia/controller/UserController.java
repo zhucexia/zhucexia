aeb9a9c20c6ba14753b866a6bbad968a6e63dbfc
leslie.li <leslie.li@live.com>
2015-09-18 16:46:54 +0800

    网站整体技术架构 会员相关代码

---
+++ src/main/java/com/keji50/zhucexia/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.keji50.zhucexia.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 网站会员控制器
+ *
+ * @author chao.li
+ * @version
+ * @since Ver 1.1
+ * @Date 2015年9月18日 下午4:43:23
+ *
+ * @see
+ */
+
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @RequestMapping(value = "/index")
+    public String index() {
+        return "/user/index";
+    }
+
+}
---
+++ src/main/java/com/keji50/zhucexia/dao/mapper/UserPoMapper.java
@@ -0,0 +1,28 @@
+/**
+ * UserPoMapper.java
+ * com.keji50.zhucexia.dao.mapper
+ *
+ * Function： TODO 
+ *
+ *   ver     date      		author
+ * ──────────────────────────────────
+ *   		 2015年9月18日 		chao.li
+ *
+ * Copyright (c) 2015, Howbuy Rights Reserved.
+*/
+
+package com.keji50.zhucexia.dao.mapper;
+/**
+ * 会员对象数据库访问接口
+ *
+ * @author   chao.li
+ * @version  
+ * @since    Ver 1.1
+ * @Date	 2015年9月18日		下午4:24:05
+ *
+ * @see 	 
+ */
+public interface UserPoMapper {
+
+}
+
