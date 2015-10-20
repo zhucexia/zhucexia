849030cf94fc918ba17c06126d8ae1859f6ef918
leslie.li <leslie.li@live.com>
2015-10-20 17:26:30 +0800

    更新测试代码

--- src/main/java/com/keji50/zhucexia/web/controller/PaymentController.java
+++ src/main/java/com/keji50/zhucexia/web/controller/PaymentController.java
@@ -1,7 +1,10 @@
 package com.keji50.zhucexia.web.controller;
 
+import java.text.SimpleDateFormat;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,7 +25,7 @@ public class PaymentController {
 		// 支付宝支付
 		if ("alipay".equalsIgnoreCase(paymentCode)) {
 			// 数据库中查询获取订单详情， 拼装支付宝支付所需参数
-			request.setAttribute("WIDout_trade_no", "20151020102010228101822910"); // 订单号
+			request.setAttribute("WIDout_trade_no", String.valueOf(System.currentTimeMillis())); // 订单号
 			request.setAttribute("WIDsubject", "测试订单");                           // 订单名称
 			request.setAttribute("WIDtotal_fee", "0.01");                            // 订单金额
 			request.setAttribute("WIDbody", "测试订单描述");                           // 订单描述
